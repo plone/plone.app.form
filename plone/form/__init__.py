cf1035fa6b5b83f2de4077c624ba4fba14f39700
Rocky Burt <rocky@serverzen.com>
2006-07-13 17:34:43 +0000

    Cleaned up line breaks a bit.
    svn path=/plone.form/trunk/; revision=10253

--- plone/form/__init__.py
+++ plone/form/__init__.py
@@ -1,6 +1,7 @@
 from Products.Five.browser import pagetemplatefile
-
 from plone.form._named import named_template_adapter
 
-default_named_template_adapter = named_template_adapter(pagetemplatefile.\
-    ViewPageTemplateFile('pageform.pt'))
+__all__ = ('named_template_adapter', 'default_named_template')
+
+_template = pagetemplatefile.ViewPageTemplateFile('pageform.pt')
+default_named_template_adapter = named_template_adapter(_template)
