5edb19c60976e96b7b75c341b6b834eb05288dc6
Rocky Burt <rocky@serverzen.com>
2006-07-15 00:13:08 +0000

    Added a bit of cleanup and some docs.
    svn path=/plone.form/trunk/; revision=10255

--- plone/form/README.txt
+++ plone/form/README.txt
@@ -1,7 +1,24 @@
-plone.form Package Readme
-=========================
+plone.form
+==========
 
 Overview
 --------
 
+The plone.form package gives Plone the ability to better adapt common
+Zope 3 UI style functionality to a more appropriate Plone style.
 
+Features
+--------
+
+  - **Better integration with zope.formlib.**  The default templates
+    distributed with zope.formlib are table-based and don't follow
+    common Plone UI patterns, plone.form addresses this.
+
+  - **Traditional portal_skins defineable custom templates.**  Currently
+    this means that if a view component is defined with with
+    ``<browser:page template="foo.pt">`` then when that view is displayed
+    it will lookup "foo" via portal_skins first and only use the default
+    foo.pt if no overridden foo can be found.  This currently means
+    the view class must define a 'template' attribute which uses
+    a named template (which in the case of zope.formlib is always the
+    case) ... this logic is currently in the process of refinement.
--- plone/form/_named.py
+++ plone/form/_named.py
@@ -8,6 +8,10 @@ from Products.Five.browser import metaconfigure
 from Products.PageTemplates import ZopePageTemplate
 
 def proper_name(filename):
+    """Get the base name of a possibly full path and if it ends with
+    .pt or .zpt, chop it off.
+    """
+    
     basepath, ext = os.path.splitext(filename)
     name = basepath
     if ext and ext.lower() not in ('.pt', '.zpt'):
@@ -16,6 +20,12 @@ def proper_name(filename):
     return os.path.basename(name)
 
 class NamedTemplateAdapter(Acquisition.Explicit):
+    """A named template adapter implementation that has the ability
+    to lookup the template portion from regular traversal (intended for
+    being able to customize the template portion of a view component
+    in the traditional portal_skins style).
+    """
+    
     interface.implements(namedtemplate.INamedTemplate)
 
     def __init__(self, context):
@@ -25,29 +35,35 @@ class NamedTemplateAdapter(Acquisition.Explicit):
     def __call__(self, *args, **kwargs):
         view = self.context
         cleanup = []
+        
+        # basically this means we only do customized template lookups
+        # for views defined with <browser:page template='foo'> 
         if isinstance(view, metaconfigure.ViewMixinForTemplates):
             index = getattr(view, 'index', None)
             if index is not None:
                 name = proper_name(index.filename)
-                template = view.context.portal_url.getPortalObject().restrictedTraverse(name)
-                template = template.aq_base.__of__(view.context)
-
-                if isinstance(template, ZopePageTemplate.ZopePageTemplate):
-                    def _pt_context(view=view, 
-                                    template=template, 
-                                    orig_pt_getContext=template.pt_getContext):
-                        cdict = orig_pt_getContext()
-                        cdict['view'] = view
-                        return cdict
-                    template.pt_getContext = _pt_context
-                    cleanup.append('pt_getContext')
-        else:
-            template = self.default_template[0].__of__(view)
-            def context_builder(self, view=view):
-                cdict = self.pt_getContext()
-                cdict['view'] = view
-                return cdict
-            template.pt_getContext = context_builder
+                try:
+                    template = view.context.portal_url.getPortalObject().restrictedTraverse(name)
+                except AttributeError:
+                    # ok, we couldn't find a portal_skins defined item
+                    # so we fall back to the defined page template
+                    template = index
+                else:
+                    template = template.aq_base.__of__(view.context)
+    
+                    # here we dynamically monkey a page template instance
+                    # so that we can insert the view variable into
+                    # the template's global namespace... evil, but its
+                    # what we do until we find a better way
+                    if isinstance(template, ZopePageTemplate.ZopePageTemplate):
+                        def _pt_context(view=view, 
+                                        template=template, 
+                                        orig_pt_getContext=template.pt_getContext):
+                            cdict = orig_pt_getContext()
+                            cdict['view'] = view
+                            return cdict
+                        template.pt_getContext = _pt_context
+                        cleanup.append('pt_getContext')
 
         result = template(*args, **kwargs)
         for x in cleanup:
@@ -55,6 +71,10 @@ class NamedTemplateAdapter(Acquisition.Explicit):
         return result
 
 def named_template_adapter(template):
+    """Return a new named template adapter which defaults the to given
+    template.
+    """
+    
     new_class = new.classobj('GeneratedClass', 
                              (NamedTemplateAdapter,),
                              {})
