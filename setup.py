8a49a434ec47fb9c0908fc3524b56a9b49b558ca
Hanno Schlichting <hanno@hannosch.eu>
2006-09-21 12:14:46 +0000

    Added framework classifiers.
    svn path=/plone.app.form/trunk/; revision=10971

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(name='plone.app.form',
       description="",
       long_description="""\
 """,
-      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=['Framework :: Zope2'],
       keywords='',
       author='Plone Foundation',
       author_email='plone-developers@lists.sourceforge.net',
