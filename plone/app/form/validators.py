3e08504d1b412572c8d975b8126b1da59c0ad49e
Martin Aspeli <optilude@gmail.com>
2007-02-15 14:23:54 +0000

    Add a silly null-validator. Needed for "cancel" type buttons.
    svn path=/plone.app.form/branches/plone-3.0/; revision=12466

---
+++ plone/app/form/validators.py
@@ -0,0 +1,10 @@
+
+def null_validator(*args, **kwargs):
+    """A validator that doesn't validate anything.
+    
+    This is somewhat lame, but if you have a "Cancel" type button that
+    won't want to validate the form, you need something like this.
+    
+    @form.action("Cancel", validator=null_validator)
+    """
+    return {}
