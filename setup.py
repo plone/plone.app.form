e6c88451830d6fc485fb91be9649bba44eb3f33e
Martin Aspeli <optilude@gmail.com>
2007-02-15 14:43:25 +0000

    Update metadata
    svn path=/plone.app.form/branches/plone-3.0/; revision=12468

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '1.0a2'
 
 setup(name='plone.app.form',
       version=version,
-      description="",
+      description="Form integration for Plone",
       long_description="""\
+This package enables zope.formlib forms to work in Zope 2 code, styled to
+look like Plone forms. Note that developers should use zope.formlib directly,
+not Products.Five.formlib.
 """,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=['Framework :: Zope2'],
