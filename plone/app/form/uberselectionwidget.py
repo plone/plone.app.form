2c73898708e9b0386a82c387739b89fb02bf5ca6
Florian Schulze <florian.schulze@gmx.net>
2007-02-19 11:39:04 +0000

    Refactored to make simpler. UberMultiSelect basically works now.
    svn path=/plone.app.form/branches/uberselectionwidget-plip124/; revision=12679

--- plone/app/form/uberselectionwidget.py
+++ plone/app/form/uberselectionwidget.py
@@ -4,7 +4,7 @@ from zope.formlib import form
 from zope.publisher.interfaces.browser import IBrowserRequest
 
 from zope.app.form.browser.interfaces import ISourceQueryView, ITerms
-from zope.app.form.browser.source import SourceListInputWidget
+from zope.app.form.browser.widget import SimpleInputWidget
 from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
 
 from Products.CMFCore import utils as cmfutils
@@ -18,6 +18,14 @@ class IUberSelect(interface.Interface):
 
 class UberSelect(schema.Choice):
     interface.implements(IUberSelect)
+
+
+class IUberMultiSelect(interface.Interface):
+    pass
+
+
+class UberMultiSelect(schema.Choice):
+    interface.implements(IUberMultiSelect)
 
 
 class MySource(object):
@@ -37,14 +45,10 @@ class MyTerms(object):
 
     def getTerm(self, value):
         title = unicode(value)
-        try:
-            token = title.encode('base64').strip()
-        except binascii.Error:
-            raise LookupError(token)
-        return schema.vocabulary.SimpleTerm(value, token=token, title=title)
+        return schema.vocabulary.SimpleTerm(value, token=value, title=title)
 
     def getValue(self, token):
-        return token.decode('base64')
+        return token
 
 provideAdapter(
     MyTerms,
@@ -84,85 +88,137 @@ provideAdapter(
 )
 
 
-class IUberSelectionDemoForm(interface.Interface):
-    text = UberSelect(title=u'Search Text',
-                         description=u'The text to search for',
-                         required=False,
-                         source=MySource())
+class UberSelectionWidget(SimpleInputWidget):
+    _error = None
 
-
-class UberSelectionWidget(SourceListInputWidget):
     template = ViewPageTemplateFile('uberselectionwidget.pt')
 
-    def _input_value(self):
-        tokens = self.request.form.get(self.name)
-        for name, queryview in self.queryviews:
-                newtokens = self.request.form.get(name+'.selection')
-                if newtokens:
-                    if tokens:
-                        tokens = tokens + newtokens
-                    else:
-                        tokens = newtokens
+    def __init__(self, field, source, request):
+        SimpleInputWidget.__init__(self, field, request)
+        self.source = source
+        self.terms = getMultiAdapter((source, self.request), ITerms)
+        self.queryview = getMultiAdapter((source, self.request), ISourceQueryView)
 
-        if tokens:
-            remove = self.request.form.get(self.name+'.checked')
-            if remove and (self.name+'.remove' in self.request):
-                tokens = [token
-                          for token in tokens
-                          if token not in remove
-                          ]
-            value = []
-            for token in tokens:
-                try:
-                    v = self.terms.getValue(str(token))
-                except LookupError:
-                    pass # skip invalid tokens (shrug)
-                else:
-                    value.append(v)
+    def _value(self):
+        if self._renderedValueSet():
+            value = self._data
         else:
-            if self.name+'.displayed' in self.request:
-                value = []
+            token = self.request.form.get(self.name)
+
+            if token is not None:
+                try:
+                    value = self.terms.getValue(str(token))
+                except LookupError:
+                    value = self.context.missing_value
             else:
                 value = self.context.missing_value
 
-        if value:
-            r = []
-            seen = {}
-            for s in value:
-                if s not in seen:
-                    r.append(s)
-                    seen[s] = 1
-            value = r
+        return value
+
+    def hidden(self):
+        value = self._value()
+        if value == self.context.missing_value:
+            return '' # Nothing to hide ;)
+
+        try:
+            term = self.terms.getTerm(value)
+        except LookupError:
+            # A value was set, but it's not valid.  Treat
+            # it as if it was missing and return nothing.
+            return ''
+
+        return '<input type="hidden" name="%s" value="%s" />' % (self.name, term.token)
+
+    def error(self):
+        if self._error:
+            return getMultiAdapter((self._error, self.request),
+                                   IWidgetInputErrorView).snippet()
+        return ""
+
+    def __call__(self):
+        value = self._value()
+        if value is not None:
+            value = [self.terms.getTerm(x) for x in value]
+        field = self.context
+        results = []
+        qresults = self.queryview.results(self.name)
+        if qresults is not None:
+            for item in qresults:
+                results.append(self.terms.getTerm(item))
+        return self.template(field=field, results=results, name=self.name, value=value)
+
+    def getInputValue(self):
+        token = self.request.get(self.name)
+
+        field = self.context
+
+        if token is None:
+            if field.required:
+                raise zope.app.form.interfaces.MissingInputError(
+                    field.__name__, self.label,
+                    )
+            return field.missing_value
+
+        try:
+            value = self.terms.getValue(str(token))
+        except LookupError:
+            err = zope.schema.interfaces.ValidationError(
+                "Invalid value id", token)
+            raise WidgetInputError(field.__name__, self.label, err)
+
+        # Remaining code copied from SimpleInputWidget
+
+        # value must be valid per the field constraints
+        try:
+            field.validate(value)
+        except ValidationError, err:
+            self._error = WidgetInputError(field.__name__, self.label, err)
+            raise self._error
 
         return value
 
-    def queryviews(self):  
-        return [
-                    (self.name,
-                     getMultiAdapter(
-                        (self.source, self.request),
-                        ISourceQueryView,
-                     )
-                    )
-               ]
+    def hasInput(self):
+        if self.name in self.request or self.name+'.displayed' in self.request:
+            return True
 
-    queryviews = property(queryviews)
-            
-    def __call__(self):
-        print "__call__", self.request.form
-        value = self._value()
-        if value is None:
-            value = []
-        value = [self.terms.getTerm(x) for x in value]
-        print repr(value)
-        field = self.context
-        results = []
-        for name, queryview in self.queryviews:
-            qresults = queryview.results(name)
-            if qresults is not None:
-                for item in qresults:
-                    results.append(self.terms.getTerm(item))
-        return self.template(field=field, results=results, name=self.name, value=value)
+        token = self.request.form.get(self.name)
+        if token is not None:
+            return True
+
+        return False
+
+class UberMultiSelectionWidget(UberSelectionWidget):
+    template = ViewPageTemplateFile('ubermultiselectionwidget.pt')
+
+    def _value(self):
+        if self._renderedValueSet():
+            value = self._data
+        else:
+            tokens = self.request.form.get(self.name)
+
+            if tokens is not None:
+                value = []
+                for token in tokens:
+                    try:
+                        v = self.terms.getValue(str(token))
+                    except LookupError:
+                        pass # skip invalid values
+                    else:
+                        value.append(v)
+            else:
+                if self.name+'.displayed' in self.request:
+                    value = []
+                else:
+                    value = self.context.missing_value
+
+        return value
+
+
+class IUberSelectionDemoForm(interface.Interface):
+    text = UberMultiSelect(title=u'Search Text',
+                         description=u'The text to search for',
+                         required=False,
+                         source=MySource())
 
 
 class UberSelectionDemoForm(form.PageForm):
