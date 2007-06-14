38186d1d2b16bfc955b3a060ef99d0fc2585ce66
Hanno Schlichting <hanno@hannosch.eu>
2007-06-14 22:08:56 +0000

    Added a new LanguageDropdownChoiceWidget, which renders a dropdown widget which is populated by a language vocabulary. The language names are then localized based on the zope.i18n.locales information.
    svn path=/plone.app.form/branches/plone-3.0/; revision=15331

--- plone/app/form/widgets/__init__.py
+++ plone/app/form/widgets/__init__.py
@@ -1,3 +1,4 @@
 #
 from checkboxwidget import CheckBoxWidget
 from checkboxwidget import DisabledCheckBoxWidget
+from selectionwidget import LanguageDropdownChoiceWidget
---
+++ plone/app/form/widgets/selectionwidget.py
@@ -0,0 +1,25 @@
+from zope.app.form.browser.itemswidgets import DropdownWidget
+from zope.component import queryMultiAdapter
+from zope.schema.interfaces import ITitledTokenizedTerm
+
+
+class LanguageDropdownChoiceWidget(DropdownWidget):
+    """ A DropdownWidget which renders a localized language selection.
+    """
+
+    def __init__(self, field, request):
+        """Initialize the widget."""
+        super(LanguageDropdownChoiceWidget, self).__init__(field,
+            field.vocabulary, request)
+        portal_state = queryMultiAdapter((self.context, request),
+                                         name=u'plone_portal_state')
+        self.languages = portal_state.locale().displayNames.languages
+
+    def textForValue(self, term):
+        """Extract a string from the `term`.
+
+        The `term` must be a vocabulary tokenized term.
+        """
+        if ITitledTokenizedTerm.providedBy(term):
+            return self.languages.get(term.value, term.title)
+        return term.token
