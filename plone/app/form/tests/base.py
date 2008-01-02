63fe20c931dc568528fc401f8992b73194420ebc
Martin Aspeli <optilude@gmail.com>
2008-01-02 23:12:47 +0000

    Fix the test coverage and visuals
    svn path=/plone.app.form/branches/plip202-formlib-kss/; revision=18783

--- plone/app/form/tests/base.py
+++
@@ -1,8 +0,0 @@
-from Products.PloneTestCase import PloneTestCase as ptc
-ptc.setupPloneSite()
-
-class FormTestCase(ptc.PloneTestCase):
-    pass
-
-class FormFunctionalTestCase(ptc.FunctionalTestCase):
-    pass
