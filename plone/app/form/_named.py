da57bf7f2ab5366a0e7cdf5280f4a8d6074034d6
David Glick <davidglick@groundwire.org>
2008-11-10 05:51:10 +0000

    2 fixes for (getting closer to) making this work with zope 2.12: - ViewPageTemplateFile should no longer be acquisition wrapped - ViewPageTemplateFile is now a property descriptor expecting to be called on   a browser view and return a BoundPageTemplate; since the NamedTemplateAdapter   is wrapping it we need to take extra care in order to bind our   BoundPageTemplate to the real view.
    svn path=/plone.app.form/trunk/; revision=23251

--- plone/app/form/_named.py
+++ plone/app/form/_named.py
@@ -2,6 +2,7 @@ import new
 from Acquisition import aq_inner
 from zope import interface
 from zope.formlib import namedtemplate
+from Products.Five.browser.pagetemplatefile import BoundPageTemplate
 
 class NamedTemplateAdapter(object):
     """A named template adapter implementation that has the ability
@@ -23,7 +24,12 @@ class NamedTemplateAdapter(object):
         context = aq_inner(self.context)
         context_of_context = aq_inner(context.context)
         view = context.__of__(context_of_context)
-        return self.default_template.__of__(view)(*args, **kwargs)
+        
+        # self.default_template is a ViewPageTemplateFile, which is a property descriptor
+        # whose __get__ method returns a BoundPageTemplate.  That expects to be accessed from
+        # a view, but we're accessing it from a NamedTemplateAdapter so we have to be sneaky and
+        # make our own BoundPageTemplate rather than calling self.default_template directly.
+        return BoundPageTemplate(self.__class__.__dict__['default_template'], view)(*args, **kwargs)
 
 def named_template_adapter(template):
     """Return a new named template adapter which defaults the to given
