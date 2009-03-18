87462714c0a8fac4fb907cef787ff97ed81a4653
Radim Novotny <novotny.radim@gmail.com>
2009-03-18 20:38:59 +0000

    Fixed return value of the multi selection widget _value method (None raises len() of unsized object).
    svn path=/plone.app.form/branches/1.1/; revision=25999

--- plone/app/form/widgets/uberselectionwidget.py
+++ plone/app/form/widgets/uberselectionwidget.py
@@ -146,7 +146,8 @@ class UberMultiSelectionWidget(UberSelectionWidget):
                     value = []
                 else:
                     value = self.context.missing_value
-
+        if value is None:
+            value = []
         return value
 
     def _getRenderValue(self):
