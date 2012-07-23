b1acbc99f60514d79caaa577696d7a950d7ce675
Vitaliy Podoba <vitaliypodoba@gmail.com>
2012-07-23 14:25:23 +0300

    Customize formlib sequencewidget in order to add originalValue attribute. We need it to enable form unload protection for list fields on Plone forms.

--- plone/app/form/widgets/__init__.py
+++ plone/app/form/widgets/__init__.py
@@ -3,3 +3,4 @@ from checkboxwidget import CheckBoxWidget
 from checkboxwidget import DisabledCheckBoxWidget
 from multicheckboxwidget import MultiCheckBoxWidget
 from selectionwidget import LanguageDropdownChoiceWidget
+from sequencewidget import SequenceWidget, TupleSequenceWidget, ListSequenceWidget
