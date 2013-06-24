6e6c31d21943427f79a92340d9aa0fedf0e299b3
David Glick <david@glicksoftware.com>
2013-06-24 15:42:22 -0700

    decode form input, pep8

--- plone/app/form/inline_validation.py
+++ plone/app/form/inline_validation.py
@@ -4,6 +4,8 @@ from zope.formlib import form as formlib
 
 from Acquisition import aq_inner
 from Products.Five import BrowserView
+from Products.Five.browser.decode import processInputs
+from Products.Five.browser.decode import setPageEncoding
 
 
 class InlineValidationView(BrowserView):
@@ -16,12 +18,15 @@ class InlineValidationView(BrowserView):
         form = aq_inner(self.context)
         context = aq_inner(form.context)
         request = self.request
+        processInputs(request)
+        setPageEncoding(request)
 
-        raw_fname = fname[len(form.prefix)+1:]
+        raw_fname = fname[len(form.prefix) + 1:]
         formlib_field = form.form_fields[raw_fname]
  
-        widgets = formlib.setUpWidgets((formlib_field,), form.prefix, context, 
-            request, form=form, adapters={}, ignore_request=False)            
+        widgets = formlib.setUpWidgets(
+            (formlib_field,), form.prefix, context,
+            request, form=form, adapters={}, ignore_request=False)
         widget = widgets[raw_fname]
         error = None
         try:
